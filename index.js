5d494cb208b87b9213c814166470d34e781b6029
Michael Carroll <carrollm2@gmail.com>
2017-11-10 20:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ var name = "Susan"
 var height = 39
 var message = `${name} is ${height} inches tall`
 
+var word = 'bird'
+
+var firstName = "Mike"
+
+function greeting(firstName){
+  return "Hello, ${firstNmae} .Bye Mike"
+  
+}
+
+console.log(greeting(firstName))
+
+
 module.exports = {name, height, message}
